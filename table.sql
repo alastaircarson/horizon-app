82c592dec4959d03da0b6524419fc986ecf3be76
Alastair Carson <alastair.carson@thinkwhere.com>
2019-09-03 17:57:47 +0100

    Table definition

--- table.sql
+++ table.sql
@@ -1,3 +1,18 @@
+
+CREATE DATABASE viewpointdb
+  WITH OWNER = twadmin
+       ENCODING = 'UTF8'
+	   TEMPLATE = template0
+       LC_COLLATE = 'en_GB.UTF-8'
+       LC_CTYPE = 'en_GB.UTF-8'
+       CONNECTION LIMIT = -1;
+GRANT CONNECT, TEMPORARY ON DATABASE viewpointdb TO public;
+GRANT ALL ON DATABASE viewpointdb TO twadmin;
+
+CREATE EXTENSION postgis;
+
+CREATE ROLE viewpoint_user LOGIN encrypted password 'viewpoint' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+
 create table viewpoint
 (
 	id serial primary key,
@@ -9,4 +24,7 @@ create table viewpoint
 
 create index viewpoint_sdx on viewpoint using gist (geometry);
 
-insert into viewpoint(geometry, processed) values(ST_GeomFromText('POINT(279093 693777)',27700), false);
+GRANT SELECT,INSERT,UPDATE,DELETE ON table viewpoint TO viewpoint_user;
+GRANT USAGE on SEQUENCE viewpoint_id_seq to viewpoint_user;
+
+--insert into viewpoint(geometry, processed) values(ST_GeomFromText('POINT(279093 693777)',27700), false);
